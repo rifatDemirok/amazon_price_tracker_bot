d227594a21205cb98d4053270da61fdc70e815d4
rifatDemirok <rftyy111@gmail.com>
2023-03-21 12:18:19 +0300

    amazon price tracker.
    you can tracking the your product's prices on amazon account.(i am still developing this app.)(include: SQL, Selenium and many tools )

---
+++ amz_bot/deneme/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.asin_add_bt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CHECK BASE PRICE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(2, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // asin_add_bt
+            // 
+            this.asin_add_bt.Location = new System.Drawing.Point(2, 213);
+            this.asin_add_bt.Name = "asin_add_bt";
+            this.asin_add_bt.Size = new System.Drawing.Size(161, 44);
+            this.asin_add_bt.TabIndex = 2;
+            this.asin_add_bt.Text = "ADD ASIN DB";
+            this.asin_add_bt.UseVisualStyleBackColor = true;
+            this.asin_add_bt.Click += new System.EventHandler(this.asin_add_bt_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "COMPARE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.asin_add_bt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button asin_add_bt;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ amz_bot/deneme/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Mail;
+using System.Net;
+using System.Data.SQLite;
+using System.Data.SQLite.Linq;
+using System.IO;
+using System.Data.SqlClient;
+using static System.Data.Entity.Infrastructure.Design.Executor;
+
+namespace deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region
+        SQLiteCommand add_asin_cmd;
+        string asin;
+        string sql_conn_text = @"Data Source=C:\Users\Rıfat DEMİROK\Desktop\amz_bot\amz_bot.db";
+        List<string> asins_list = new List<string>();
+        int count_asins;
+        SQLiteCommand add_price_cmd;
+        SQLiteConnection connection;
+        char split_price = ' ';
+        #endregion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string get_compare_cmd = "SELECT ASIN, price_old, price_new FROM prices";
+            SQLiteConnection conn = new SQLiteConnection(sql_conn_text);
+            SQLiteCommand compare_price_ = new SQLiteCommand(get_compare_cmd, conn);
+            conn.Open();
+            SQLiteDataReader take_data = compare_price_.ExecuteReader();
+            while (take_data.Read())
+            {
+                string asin = Convert.ToString(take_data["ASIN"]);
+                listBox1.Items.Add(asin);
+            }
+        
+
+
+            //using (StreamReader sr = new StreamReader("C:\\Users\\Rıfat DEMİROK\\Desktop\\amz_bot\\amz_bot.txt"))
+            //{
+            //    asin = "";
+            //    while ((asin = sr.ReadLine()) != null)
+            //    {
+
+
+            //    }
+            //}
+
+        }
+
+        private void asin_add_bt_Click(object sender, EventArgs e)
+        {
+
+            SQLiteConnection sql_conn = new SQLiteConnection(sql_conn_text);
+            sql_conn.Open();
+            using (StreamReader sr = new StreamReader("C:\\Users\\Rıfat DEMİROK\\Desktop\\amz_bot\\amz_bot.txt"))
+            {
+                asin = "";
+                while ((asin = sr.ReadLine()) != null)
+                {
+                    asins_list.Add(asin);
+
+                    string add_asin = "INSERT INTO prices(\n";
+                    add_asin += "ASIN)\n";
+                    add_asin += "VALUES(\n";
+                    add_asin += "\t'" + asin + "'\n";
+                    add_asin += ")";
+
+                    add_asin_cmd = new SQLiteCommand(add_asin, sql_conn);
+                    add_asin_cmd.ExecuteNonQuery();
+                }
+            }
+            sql_conn.Close();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string asins_count = "SELECT COUNT (ASIN) FROM prices";
+            using (SQLiteConnection sql_conn = new SQLiteConnection(sql_conn_text))
+            {
+                SQLiteCommand asins_counter = new SQLiteCommand(asins_count, sql_conn);
+                sql_conn.Open();
+                int countDecimal = Convert.ToInt32(asins_counter.ExecuteScalar());
+                count_asins = (int)countDecimal;
+            }
+
+            for (int i = 0; i <= 49; i++)
+            {
+                ChromeDriverService servis = ChromeDriverService.CreateDefaultService();
+                servis.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--headless");
+                IWebDriver driver = new ChromeDriver(servis);
+                driver.Navigate().GoToUrl("https://www.amazon.com/dp/" + listBox1.Items[i]);
+                Thread.Sleep(500);
+
+                driver.FindElement(By.Id("glow-ingress-line2")).Click();
+                Thread.Sleep(1500);
+
+                driver.FindElement(By.CssSelector("input[type='text'][aria-label='or enter a US zip code']")).SendKeys("87010");
+                Thread.Sleep(500);
+
+                driver.FindElement(By.Id("GLUXZipUpdate")).Click();
+                Thread.Sleep(500);
+
+                driver.Navigate().Refresh();
+                Thread.Sleep(1000);
+
+                IWebElement price_whole = driver.FindElement(By.ClassName("a-price-whole"));
+                IWebElement price_fraction = driver.FindElement(By.ClassName("a-price-fraction"));
+                Thread.Sleep(500);
+
+                double price = 0.0;
+                if (double.TryParse(price_whole.Text + "." + price_fraction.Text, out price))
+                {
+                    // price değeri başarıyla dönüştürüldü
+                }
+                else
+                {
+                    IWebElement price_2nd = driver.FindElement(By.Id("sns-base-price"));
+                    Thread.Sleep(500);
+                    string price2nd = price_2nd.Text;
+                    string[] price_split_index = price2nd.Split(split_price);
+                    string sbstr_price = price_split_index[0];
+                    price = Convert.ToDouble(sbstr_price.Substring(1, sbstr_price.Length - 1));
+
+
+                }
+
+                SQLiteConnection connection = new SQLiteConnection(sql_conn_text);
+                connection.Open();
+
+                string add_price = "UPDATE prices SET price_old =" + price + " WHERE ASIN = " + "'" + listBox1.Items[i] + "'";
+                SQLiteCommand add_price_cmd = new SQLiteCommand(add_price, connection);
+                add_price_cmd.ExecuteNonQuery();
+                driver.Quit();
+
+
+
+
+
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string asins_count = "SELECT COUNT (ASIN) FROM prices";
+            using (SQLiteConnection sql_conn = new SQLiteConnection(sql_conn_text))
+            {
+                SQLiteCommand asins_counter = new SQLiteCommand(asins_count, sql_conn);
+                sql_conn.Open();
+                int countDecimal = Convert.ToInt32(asins_counter.ExecuteScalar());
+                count_asins = (int)countDecimal;
+            }
+
+            for (int i = 0; i <= listBox1.Items.Count; i++)
+            {
+                ChromeDriverService servis = ChromeDriverService.CreateDefaultService();
+                servis.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--headless");
+                IWebDriver driver = new ChromeDriver(servis);
+                driver.Navigate().GoToUrl("https://www.amazon.com/dp/" + listBox1.Items[i]);
+                Thread.Sleep(500);
+
+                driver.FindElement(By.Id("glow-ingress-line2")).Click();
+                Thread.Sleep(1500);
+
+                driver.FindElement(By.CssSelector("input[type='text'][aria-label='or enter a US zip code']")).SendKeys("87010");
+                Thread.Sleep(500);
+
+                driver.FindElement(By.Id("GLUXZipUpdate")).Click();
+                Thread.Sleep(500);
+
+                driver.Navigate().Refresh();
+                Thread.Sleep(1000);
+
+                IWebElement price_whole = driver.FindElement(By.ClassName("a-price-whole"));
+                IWebElement price_fraction = driver.FindElement(By.ClassName("a-price-fraction"));
+                Thread.Sleep(500);
+
+                double price = 0.0;
+                if (double.TryParse(price_whole.Text + "." + price_fraction.Text, out price))
+                {
+                    // price değeri başarıyla dönüştürüldü
+                }
+                else
+                {
+                    IWebElement price_2nd = driver.FindElement(By.Id("sns-base-price"));
+                    Thread.Sleep(500);
+                    string price2nd = price_2nd.Text;
+                    string[] price_split_index = price2nd.Split(split_price);
+                    string sbstr_price = price_split_index[0];
+                    price = Convert.ToDouble(sbstr_price.Substring(1, sbstr_price.Length - 1));
+                }
+
+
+
+                string add_price = "UPDATE prices SET price_new =" + price + " WHERE ASIN = " + "'" + listBox1.Items[i] + "'";
+                SQLiteCommand add_price_cmd = new SQLiteCommand(add_price, connection);
+                add_price_cmd.ExecuteNonQuery();
+                driver.Quit();
+            }
+
+
+
+            for (int i = 0; i <= listBox1.Items.Count; i++)
+            {
+                SQLiteConnection connection = new SQLiteConnection(sql_conn_text);
+
+
+                string get_compare_cmd = "SELECT ASIN, price_old, price_new FROM prices WHERE ASIN=" + "\"" + listBox1.Items[i] + "\" ";
+                SQLiteConnection conn = new SQLiteConnection(sql_conn_text);
+                SQLiteCommand compare_price_ = new SQLiteCommand(get_compare_cmd, conn);
+                conn.Open();
+                SQLiteDataReader take_data = compare_price_.ExecuteReader();
+                take_data.Read();
+                double new_price = Convert.ToDouble(take_data["price_new"]);
+                string asin = Convert.ToString(take_data["ASIN"]);
+
+                double old_price = Convert.ToDouble(take_data["price_old"]);
+
+                if (old_price > new_price)
+                {
+                    MailMessage message = new MailMessage();
+                    SmtpClient sc = new SmtpClient("smtp.outlook.com");
+                    sc.Credentials = new NetworkCredential("xxxexample@outlook.com", "xxxPASSWORDxxx.");
+                    sc.Port = 587;
+                    sc.Host = "smtp.outlook.com";
+                    sc.EnableSsl = true;
+                    SmtpClient server = new SmtpClient("outlook.com");
+                    message.From = new MailAddress("xxxexample@outlook.com");
+                    message.To.Add("xxxexm@gmail.com");
+                    message.Subject = asin + " ASİN KODLU ÜRÜNÜN FİYATI DÜŞTÜ";
+                    message.Body = "FİYATI " + old_price + " OLAN" + asin + " ÜRÜNÜNÜN FİYATI " + new_price + " E DÜŞTÜ. LİNK ; " + "https://www.amazon.com/dp/" + asin;
+                    sc.Send(message);
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
+
